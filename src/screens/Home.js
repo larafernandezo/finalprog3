fd5954f5d75d22b11ca342981a48055cd96c0b8d
marucalliera <mcalliera@udesa.edu.ar>
2022-11-14 16:12:24 -0300

    agregue screen de componentes, sigo con las notas

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -39,6 +39,8 @@ class Home extends Component {
 // por q estamos desestructurando item derenderItem. Por eso depsues lo que hago es pasarle a ese item como informacion al posteo 
 //el componente post le paso todo lo que yo recibo en ese elemnto item 
 //como llega por props despues lo puedo procesar. Vamos al componente post ahora. solo para el ejemplo de la clase
+//Vamos a hacer dos cosas con la flatlist vamoos a armar una lista cortita para q retorne una lista de texto
+//despues vamos a hacer que esa flatlist renderice ottrocomponente  al cual le vamos a pasar ptops para q se puedan ver 
 //
     render(){
         console.log(this.state.posts);
@@ -46,7 +48,8 @@ class Home extends Component {
             <>
                 <Text> Home</Text>
                 <Text> Lista de posteos </Text>
-                
+         
+         
                     <FlatList 
                         data={this.state.posts}
                         keyExtractor={ onePost => onePost.id.toString()}
