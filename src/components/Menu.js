c011471c64f0250e90660d291ebad71f769457d9
m-lombardi <56735001+m-lombardi@users.noreply.github.com>
2022-11-24 06:49:36 -0800

    paraestarenlomismo

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -11,7 +11,7 @@ const Tab = createBottomTabNavigator();
 function Menu () {
     return(
         <Tab.Navigator screenOptions = {{ tabBarShowLabel: false}}>
-                <Tab.Screen name = "Home" component={ Home } options ={ { tabBarIcon: () => <Feather name="home" size={24} color="#b8eb91" /> } }  />
+            <Tab.Screen name = "Home" component={ Home } options ={ { tabBarIcon: () => <Feather name="home" size={24} color="#b8eb91" /> } }  />
             <Tab.Screen name = "Postear" component={ NuevoPost }  options ={{ tabBarIcon: () => <Feather name="plus-circle" size={24} color="#ce68fd" /> }}/>
             <Tab.Screen name = "Perfil" component={ MiPerfil } options={{ tabBarIcon: () => <MaterialIcons name="tag-faces" size={24} color="#fa9c3c" /> }}/>
         </Tab.Navigator>
